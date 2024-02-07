cb0c7529add7aaf79a93918256798594d1d80224
rathoreashish146 <117078265+rathoreashish146@users.noreply.github.com>
2024-02-07 13:21:45 +0000

    third commit

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 from flask import Flask,render_template 
 
+
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':' Bengaluru, India',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':' Delhi, India',
+    'salary':'Rs. 15,00,000'
+  },
+{
+  'id':3,
+  'title':'Frontend Engineer',
+  'location':' Remote',
+  'salary':'Rs. 12,00,000'
+},
+{
+  'id':4,
+  'title':'Backend Engineer',
+  'location':' San Francisco, USA',
+  'salary':'$120,000'
+}
+]
 app = Flask(__name__)
 # print(__name__)
 @app.route('/')  
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS,company_name='Tech Sarathi')
 
 print(__name__)
 if __name__ == "__main__":
